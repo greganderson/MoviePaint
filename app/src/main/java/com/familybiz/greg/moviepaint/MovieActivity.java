9b063c257fd296abf550816eca227f553453abcb
Greg Anderson <greg@infobytes.com>
2014-10-03 20:22:57 -0600

    Initial commit

---
+++ app/src/main/java/com/familybiz/greg/moviepaint/MovieActivity.java
@@ -0,0 +1,14 @@
+package com.familybiz.greg.moviepaint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MovieActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+    }
+}
